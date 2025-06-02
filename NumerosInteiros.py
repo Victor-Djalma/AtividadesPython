f670ca50a9760a97d49c5fb5909c7f5de050311c
Victor Djalma <victor.dj6734@gmail.com>
2025-06-02 00:27:47 -0300

    Create NumerosInteiros.py

---
+++ NumerosInteiros.py
@@ -0,0 +1,15 @@
+import math
+
+num1 = int(input("Dígite o primeiro número: "))
+num2 = int(input("Dígite o segundo número: "))
+
+soma = num1 + num2
+print(f"A Soma entre {num1} + {num2} = {soma} ")
+subtracao = num1 - num2
+print(f"A subtração entre {num1} - {num2} =  {subtracao} ")
+multiplicacao = num1 * num2
+print(f"A Multiplicação entre {num1} x {num2} =  {multiplicacao} ")
+divisao = num1 / num2
+print(f"A Divisão entre {num1} ÷ {num2} =  {divisao} ")
+
+
