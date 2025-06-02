f06aa6b053bb8376d7359c28d7ed49ad7be7fd74
Victor Djalma <victor.dj6734@gmail.com>
2025-06-02 00:26:29 -0300

    Create Conversão.py

---
+++ Conversão.py
@@ -0,0 +1,5 @@
+import math
+
+celsius = int(input("Dígite o número da temperatura: "))
+fahrenheit = celsius * 1.8 ++ 32
+print(f"Sua temperatura de {celsius}°C em fahrenheit equivale á {fahrenheit}°F ")
